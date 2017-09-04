0abba6172b7c2a9f31ce8bbf43c0aed782ed91ad
Squids Zyd <squidroidnxs@gmail.com>
2017-09-04 17:13:30 +0800

    34 + 81

---
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,24 @@
+//  Tags:   [array][binary search]
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1, mid;
+        while(lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if(nums[mid] == target) return true;
+            if((nums[lo] == nums[mid]) && (nums[hi] == nums[mid])) {
+                ++lo;
+                --hi;
+            }
+            else if(nums[lo] <= nums[mid]){
+                if((nums[lo] <= target) && (nums[mid] > target)) hi = mid - 1;
+                else lo = mid + 1;
+            }
+            else{
+                if((nums[mid] < target) && (nums[hi] >= target)) lo = mid + 1;
+                else hi = mid - 1;
+            }
+        }
+        return false;
+    }
+};
